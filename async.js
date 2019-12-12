70a47405452e85e8226da02b53416404df4ec0a1
Pelevina Anastasia <pelevina.2000@mail.ru>
2019-12-12 17:54:49 +0500

    fix

--- async.js
+++ async.js
@@ -20,37 +20,31 @@ function runParallel(jobs, parallelNum, timeout = 1000) {
     }
 
     const result = new Array(jobs.length);
-    let currentJob = 0;
-    const startJobs = jobs.splice(0, parallelNum);
 
-    const processJob = function (resolve, res, index) {
-        result[index] = res;
-        if (!hasEmptySlot(result)) {
-            resolve(result);
-        }
-    };
-
-    const runOnePromise = function (job, resolve, index) {
-        let timeOutPromise = new Promise((timeOutResolve) =>
+    const runOnePromise = function (resolve, index) {
+        const timeOutPromise = new Promise((timeOutResolve) =>
             setTimeout(() => timeOutResolve(new Error('Promise timeout')), timeout)
         );
-        Promise.race([job(), timeOutPromise])
+        Promise.race([jobs[index](), timeOutPromise])
             .then(res => {
-                processJob(resolve, res, index);
+                result[index] = res;
             }, res => {
-                processJob(resolve, res, index);
+                result[index] = res;
             })
             .finally(() => {
-                if (jobs.length !== 0) {
-                    runOnePromise(jobs.shift(), resolve, currentJob++);
+                if (index < jobs.length - 1) {
+                    runOnePromise(resolve, ++index);
+                }
+                if (!hasEmptySlot(result)) {
+                    resolve(result);
                 }
             });
     };
 
     return new Promise((resolve) => {
-        startJobs.forEach((job) => {
-            runOnePromise(job, resolve, currentJob++);
-        });
+        for (let i = 0; i < parallelNum; i++) {
+            runOnePromise(resolve, i);
+        }
     });
 }
 
